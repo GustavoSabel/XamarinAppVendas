bf1e3387970142503b46a61c13244ebb8f60d7dc
Gustavo Sabel <gustavo.sabel.gs@gmail.com>
2019-11-26 23:41:38 -0300

    Aplicando tema escuro no app

---
+++ AppVendas/AppVendas/Views/Controls/LabelEmptyView.xaml.cs
@@ -0,0 +1,22 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppVendas.Views.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LabelEmptyView : ContentView
+    {
+        public static readonly BindableProperty TextoProperty = BindableProperty.Create(nameof(Texto), typeof(string), typeof(LabelEmptyView), string.Empty);
+        public string Texto
+        {
+            get => (string)GetValue(TextoProperty);
+            set => SetValue(TextoProperty, value);
+        }
+
+        public LabelEmptyView()
+        {
+            InitializeComponent();
+        }
+    }
+}
