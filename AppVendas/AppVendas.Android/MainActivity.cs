4cdb30f881aa12cc645dd172b3609900e43fcf3c
Gustavo Sabel <gustavo.sabel.gs@gmail.com>
2019-12-07 19:55:20 -0300

    Alterada cor do status bar para preto

--- AppVendas/AppVendas.Android/MainActivity.cs
+++ AppVendas/AppVendas.Android/MainActivity.cs
@@ -2,8 +2,6 @@
 using Android.Content.PM;
 using Android.OS;
 using Android.Runtime;
-using AppVendas.Droid;
-using Xamarin.Forms;
 
 namespace AppVendas.Droid
 {
@@ -21,6 +19,8 @@ namespace AppVendas.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+
+            Window.SetStatusBarColor(Android.Graphics.Color.Argb(255, 0, 0, 0));
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
