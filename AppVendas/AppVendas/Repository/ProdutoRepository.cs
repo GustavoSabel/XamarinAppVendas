ee80c869cfa4efb14e93970fd2eec3e5aba6d411
Gustavo Sabel <gustavo@mhtec.com.br>
2020-03-12 12:03:30 -0300

    Corrigido nomes dos repositorios

--- AppVendas/AppVendas/Repository/ProdutoRepository.cs
+++ AppVendas/AppVendas/Repository/ProdutoRepository.cs
@@ -1,14 +1,9 @@
 ﻿using AppVendas.Models;
 using AppVendas.Services;
-using System.Threading.Tasks;
 
 namespace AppVendas.Repository
 {
-    public class UsuarioRepository : RepositoryBase<Usuario>
+    public class ProdutoRepository : RepositoryBase<Produto>
     {
-        public Task<Usuario> ObterPeloNomeUsuario(string nomeUsuario)
-        {
-            return Database.FindWithQueryAsync<Usuario>($"SELECT * FROM Usuario WHERE nomeUsuario = ?", nomeUsuario);
-        }
     }
 }
--- AppVendas/AppVendas/Repository/UsuarioRepository.cs
+++ AppVendas/AppVendas/Repository/UsuarioRepository.cs
@@ -1,9 +1,14 @@
 ﻿using AppVendas.Models;
 using AppVendas.Services;
+using System.Threading.Tasks;
 
 namespace AppVendas.Repository
 {
-    public class ProdutoRepository : RepositoryBase<Produto>
+    public class UsuarioRepository : RepositoryBase<Usuario>
     {
+        public Task<Usuario> ObterPeloNomeUsuario(string nomeUsuario)
+        {
+            return Database.FindWithQueryAsync<Usuario>($"SELECT * FROM Usuario WHERE nomeUsuario = ?", nomeUsuario);
+        }
     }
 }
